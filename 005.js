f7163603d602d3b2109f07abe8aa55ec22611509
arisupriatna <arisupriatna703@gmail.com>
2018-05-18 21:05:02 +0700

    add 005.js

---
+++ 005.js
@@ -0,0 +1,9 @@
+function dw(input){
+  console.log('DWDW          DW          DW') 
+  console.log('DW  DW        DW          DW')
+  console.log('DW    DW      DW    DW    DW')
+  console.log('DW    DW      DW  DWDWDW  DW')
+  console.log('DW  DW        DWDW      DWDW')
+  console.log('DWDW          DW          DW')
+}
+dw();
